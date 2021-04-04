53ad2473e62620ef014c88bbb8ab3a3f895930e5
MDmubarak786 <mohammedmubarakmk@gmail.com>
2021-04-04 21:25:05 +0530

    one

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const testData = [
+  { name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook" },
+  { name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu" },
+  { name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook" },
+];
+const List1 = (props) => (
+  <div>
+    {props.profiles.map(values1 => <Names {...values1} />)}
+
+  </div>
+);
+class Names extends Component {
+  render() {
+    const profile = this.props;
+    return (
+      <div>
+        <h3>{profile.name}</h3>
+        <img src={profile.avatar_url} height="220px"></img>
+        <h4>{profile.company}</h4>
+        <h1>-------------------</h1>
+      </div>
+    );
+  }
+}
+class Search extends Component {
+  render() {
+    const handle = (event) => {
+      event.preventDefault();
+      console.log("hello")
+    }
+    return (
+      <div>
+        <form>
+          <label>Name : </label>
+          <input type="text" placeholder="name pls" />
+          <button>Submit</button>
+        </form>
+      </div>
+    )
+  }
 }
 
-export default App;
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <h1>{this.props.title}</h1>
+
+      </div>
+
+    )
+  }
+}
+
+// All component here
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header title="Hello Github Users" />
+        <Search />
+        <List1 profiles={testData} />
+      </div>
+    )
+  }
+}
+
+
+export default App
